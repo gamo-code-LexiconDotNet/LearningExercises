e6727edee75c28894087b67bf97a204266b369eb
Gabriel Molander <gabriel.molander@gmail.com>
2021-12-09 11:37:01 +0100

    finished exercise 0

---
+++ LearningExercises/Exercises.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningExercises
+{
+    public partial class Program
+    {
+        private static void RunExerciseOne()
+        {
+            Console.WriteLine("You successfully ran exercise one!");
+        }
+
+        private static void RunExerciseTwo()
+        {
+            Console.WriteLine("You successfully ran exercise two!");
+        }
+    }
+}
--- LearningExercises/Program.cs
+++ LearningExercises/Program.cs
@@ -2,11 +2,49 @@
 
 namespace LearningExercises
 {
-    internal class Program
+    public partial class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var keepAlive = true;
+            while (keepAlive)
+            {
+                try
+                {
+                    Console.Write("Enter assignment number (or -1 to exit): ");
+                    var assignmentChoice = int.Parse(Console.ReadLine() ?? "");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    switch (assignmentChoice)
+                    {
+                        case 1:
+                            RunExerciseOne();
+                            break;
+                        case 2:
+                            RunExerciseTwo();
+                            break;
+                        case 3:
+                            // Call your next assignment here
+                            break;
+                        case -1:
+                            keepAlive = false;
+                            break;
+                        default:
+                            Console.ForegroundColor= ConsoleColor.Red;
+                            Console.WriteLine("That is not a valid assignment number!");
+                            break;
+                    }
+                    Console.ResetColor();
+                    Console.WriteLine("Hit any key to continue!.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("That is not a valid assignment number!");
+                    Console.ResetColor();
+                }
+            }
         }
     }
 }
